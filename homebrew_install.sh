a59a1880e752720b93a917518f80b754402a4473
Takumi Ishiyama <mt.stone20@gmail.com>
2017-08-11 17:57:08 +0900

    シェルスクリプトの内容を変更

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -1,3 +1,93 @@
 #!/bin/bash
 
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade..."
+
+brew upgrade --all
+
+formulas=(
+  autoconf
+  freetype
+  ghostscript
+  jemalloc
+  libtiff
+  little-cms2
+  openssl
+  rbenv
+  xz
+  automake
+  gdbm
+  git
+  jpeg
+  libtool
+  lua
+  pandoc
+  ruby-build
+  zsh
+  cabal-install
+  gettext
+  gmp
+  libpng
+  libuv
+  msgpack
+  pcre
+  unibilium
+  cmake
+  ghc
+  imagemagick
+  libtermkey
+  libvterm
+  neovim
+  pkg-config
+  vim
+)
+
+"brew tap..."
+brew tap caskroom/cask
+brew tap homebrew/core
+brew tap neovim/neovim
+
+echo "start brew install apps..."
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+casks=(
+  dropbox
+  evernote
+  skitch
+  google-chrome
+  slack
+  alfred
+  iterm2
+  virtualbox
+  vagrant
+  vagrant-manager
+  karabiner
+  shiftit
+  cheatsheet
+)
+
+echo "start brew cask install apps..."
+for cask in "${casks[@]}"; do
+    brew cask install $cask
+done
+
+brew cleanup
+brew cask cleanup
+
+cat << END
+
+**************************************************
+HOMEBREW INSTALLED! bye.
+**************************************************
+
+END
