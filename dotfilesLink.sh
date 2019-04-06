db6f4f9a3cc0b8613dc7dce428e208865e4b99ca
Takumi Ishiyama <mt.stone20@gmail.com>
2019-04-06 16:00:18 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
