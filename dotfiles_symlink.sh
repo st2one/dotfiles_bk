f6c597be61c4e3cbac282fe9d9389344c51df23a
Takumi Ishiyama <mt.stone20@gmail.com>
2019-07-28 17:07:31 +0900

    Add script and update for new mac

---
+++ dotfiles_symlink.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -u
+
+DIR="$(cd "$(dirname $0)" && pwd)"
+cd "$DIR"
+
+function link_dotfiles() {
+  local files=(
+    .ctags
+    .gitconfig
+    .gitmessage.txt
+    .ideavimrc
+    .tmux.conf
+    .vimrc
+    .zshrc
+    .peco/config.json
+    .config/nvim/init.vim
+    .config/tmux/get_buttery_tmux.sh
+    .config/karabiner/karabiner.json
+  )
+
+  for file in files[@]; do
+    local src="$DIR/$file"
+    local dst="$HOME/$file"
+
+    echo "Creating symlink: $src => $dst"
+
+    local dir="$(dirname $file)"
+    local dst_dir="$HOME/$dir"
+    [[ ! -d "$dst_dir" ]] && mkdir -p "$dst_dir"
+
+    ln -sf "$src" "$dst"
+  done
+}
+
+function run() {
+  link_dotfiles
+}
+
+run
