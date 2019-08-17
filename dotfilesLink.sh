3dddf85235b4e732e3fb403ec422c18b72a82c37
Takumi Ishiyama <mt.stone20@gmail.com>
2019-08-17 15:21:20 +0900

    :fire:remove unnecessary files for setup

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
